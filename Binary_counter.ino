8058a18cb28351a49fb7043ae032406513b9ad06
jpieczar <50695462+jpieczar@users.noreply.github.com>
2020-12-12 23:47:01 +0200

    Add files via upload

---
+++ Binary_counter.ino
@@ -0,0 +1,28 @@
+int num = 1;
+
+void setup() {
+  int i = 6;
+  while (i < 14)
+  {
+    pinMode(i, OUTPUT);
+    i++;
+  }
+}
+
+void loop() {
+  if (num == 256)
+    return (0);
+  int hold = num;
+  int keep = 6;
+  while (hold > 0)
+  {
+    if ((hold % 2) == 1)
+      digitalWrite(keep, HIGH);
+    else
+      digitalWrite(keep, LOW);
+    hold /= 2;
+    keep++;
+  }
+  num++;
+  delay(1000);
+}
